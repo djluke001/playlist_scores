0300548707f9759056d9bebd318a0b43a7669769
djluke001 <mdjluke@gmail.com>
2022-01-20 22:42:07 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+import os
+
+Path="C:\Users\djluke\Desktop\leaderboard checker"
+
+class leaderboard:
+    def __init__(self, playlist_file_name,player_1_id,player_2_id):
+        self.playlist=open(playlist_file_name)
+
+    def grab_leaderboards():
+        self.playlist_list=self.playlist.split(",")
+
+
+
+print(Path)
+x=leaderboard((Path + "playlist.bplist"),"Null","Null")
+x.grab_leaderboards()
+
